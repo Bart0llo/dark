1fb5e643c59009491c6871ed967aa048b25f6340
Ricki_Dark <77613195+Pufaek@users.noreply.github.com>
2021-03-28 18:57:03 +0200

    Update help.js

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -25,7 +25,7 @@ module.exports = {
         .setColor("#00FF00")
         .setAuthor(`Help Menu`, "https://cdn.discordapp.com/emojis/823695577288015902.png?v=1")
         .setThumbnail("https://cdn.discordapp.com/emojis/821872253682581565.png?v=1")
-        .setDescription('\n> <:member:822198810306412594> **Witaj!,** Jestem prywatnym botem Serwera **Dark.Com**\n\n> <a:BotDeveloper:823693389459226716>｜**Komendy Developerskie:**\n> `eval`\n\n> <:shield:823693241429655583>｜**Komendy Administratorskie:**\n> `say`**,**`ankieta`\n\n> <:member:822198810306412594>｜**Komendy dostępne dla członków:**\n> **`uptime`**,**`nakladka`**,**`link`**,**`theme`**,**`ping`**,**`avatar`**')
+        .setDescription('\n> <:member:822198810306412594> **Witaj!,** Jestem prywatnym botem Serwera **Dark.Com**\n\n> <a:BotDeveloper:823693389459226716>｜**Komendy Developerskie:**\n> `eval`\n\n> <:shield:823693241429655583>｜**Komendy Administratorskie:**\n> `say`**,**`ankieta`\n\n> <:member:822198810306412594>｜**Komendy dostępne dla członków:**\n> `uptime`**,**`nakladka`**,**`link`**,**`theme`**,**`ping`**,**`avatar`**')
         .setFooter(`Wykonałem dla: ${message.author.tag}`, message.author.displayAvatarURL( ({dynamic: true})))
         message.channel.startTyping();
         setTimeout(function(){
