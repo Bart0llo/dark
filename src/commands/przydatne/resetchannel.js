6cbae3c3da44f2edc4fa5e3a7d7e7edc06cffef9
Ricki_Dark <77613195+Pufaek@users.noreply.github.com>
2021-04-04 18:37:55 +0200

    Update resetchannel.js

--- src/commands/przydatne/resetchannel.js
+++ src/commands/przydatne/resetchannel.js
@@ -15,9 +15,11 @@ module.exports = {
         await message.channel.delete();
 
         const e = new Discord.MessageEmbed()
+        .setAuthor("**Bum!**", 'https://cdn.discordapp.com/emojis/821871608347885568.gif?v=1')
         .setColor('#ff0000')
         .setDescription(`**Kanał został rozjebany przez:** ${message.author.tag}`)  
         .setImage("https://cdn.discordapp.com/attachments/821868906791698446/828304189704634378/gif-explosion-73.gif")
+        .setFooter(`Wykonałem dla: ${message.author.tag}`, message.author.displayAvatarURL( ({dynamic: true})))
        
         if (channel) channel.send(e);
     }
