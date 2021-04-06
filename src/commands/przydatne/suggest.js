6e7c14d14ba096716840793ff8bd1baca26ddef4
Ricki_Dark <77613195+Pufaek@users.noreply.github.com>
2021-04-06 19:08:10 +0200

    Create suggest.js

---
+++ src/commands/przydatne/suggest.js
@@ -0,0 +1,30 @@
+const { MessageEmbed } = require('discord.js')
+
+
+module.exports = {
+    name: "suggest",
+    description: "suggestion",
+    category: "członkowie",
+    botPermission: [],
+    authorPermission: [],
+    aliases: ['suggest','s'],
+    usage: "suggest <text>",
+    ownerOnly: false,
+    run: async (client, message, args, user, text, prefix) => {
+    const channel = message.guild.channels.cache.find(c => c.name === '💡┇propozycje')
+    if(!channel) return message.channel.send('<a:nope:821871620502323280> Nie znalazłem kanału');
+
+    let messageArgs = args.join(' ');
+    const embed = new MessageEmbed()
+    .setColor('RANDOM')
+    .setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+    .setDescription(messageArgs)
+    .setFooter(`Oceń zaznaczając reakcje!`);
+
+    channel.send(embed).then((msg) =>{
+        msg.react('<:VoteUp:828032799985762365>');
+        msg.react('<:VoteDown:828032827085029407>');
+        message.channel.send('<a:takgif:821871608347885568> Propozycja wysłana!');
+        message.delete();
+    
+    })
