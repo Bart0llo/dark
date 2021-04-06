561fb732bf9eb3f30c324f94848d05a329c6516b
Ricki_Dark <77613195+Pufaek@users.noreply.github.com>
2021-04-06 19:40:26 +0200

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -44,7 +44,7 @@ module.exports = async (client) => {
         
           
           setInterval(() => {
-            activities[2] = { name: `@No Name 😍`, type: 'WATCHING' }; 
+            activities[2] = { name: `@Chill Zone Poland 😍`, type: 'WATCHING' }; 
             activities[3] = { name: `Odwiedziło nas: ${client.users.cache.size} osób`, type: 'WATCHING' }; 
             activities[4] = { name: `Cześć mogę cię zjeść?`, type: 'WATCHING' };
             if (activity > 4) activity = 0;
